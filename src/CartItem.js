c19b018ace04a4297d8b0fcb4d2dc010c680fdd1
Fluke <guyguy5050@gmail.com>
2022-09-27 19:10:32 +0700

    Add increased Quantity function button

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,22 +1,49 @@
 import React from 'react';
+import fontawesome from '@fortawesome/fontawesome';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus, faMinus, faTrash } from '@fortawesome/fontawesome-free-solid';
 
-class CartItem extends React.Component{
-  render(){
+fontawesome.library.add(faPlus, faMinus, faTrash);
+
+class CartItem extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      price: 999,
+      title: 'Phone',
+      qty: 1,
+      img: '',
+    };
+  }
+
+  increasedQuantity = () => {
+    console.log('this', this.state);
+  };
+
+  render() {
+    const { price, title, qty } = this.state;
     return (
-      <div className='cart-item'>
-        <div className='left-block'>
+      <div className="cart-item">
+        <div className="left-block">
           <img style={styles.image} />
         </div>
-        <div className='right-block'>
-          <div style={{fontSize: 25}}>Phone</div>
-          <div style={{color: '#777'}}>10,000 Bath</div>
-          <div style={{color: '#777'}}>Qty: 1</div>
-          <div className='cart-item-actions'>
+        <div className="right-block">
+          <div style={{ fontSize: 25 }}>{title}</div>
+          <div style={{ color: '#777' }}>{price} Bath</div>
+          <div style={{ color: '#777' }}>Qty: {qty}</div>
+          <div className="cart-item-actions">
             {/* Buttons */}
+            <FontAwesomeIcon
+              icon="plus"
+              className="action-icons"
+              onClick={this.increasedQuantity}
+            />
+            <FontAwesomeIcon icon="minus" className="action-icons" />
+            <FontAwesomeIcon icon="trash" className="action-icons" />
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
@@ -24,8 +51,8 @@ const styles = {
   image: {
     height: 110,
     width: 110,
-    borderRadius: 4
-  }
-}
+    borderRadius: 4,
+  },
+};
 
 export default CartItem;
